373b16d781d7eb401b8845a46442f3cabeeb8b25
Tsvetkov <zwetkow7@gmail.com>
2023-08-04 09:47:20 +0300

    fix homework

--- 1.1-first-project/first_project/app/views.py
+++ 1.1-first-project/first_project/app/views.py
@@ -4,7 +4,6 @@ from datetime import datetime
 import os
 
 
-
 def home_view(request):
     template_name = 'app/home.html'
     # впишите правильные адреса страниц, используя
@@ -14,7 +13,7 @@ def home_view(request):
         'Показать текущее время': reverse('time'),
         'Показать содержимое рабочей директории': reverse('workdir')
     }
-    
+
     # context и параметры render менять не нужно
     # подбробнее о них мы поговорим на следующих лекциях
     context = {
@@ -24,19 +23,18 @@ def home_view(request):
 
 
 def time_view(request):
-    # обратите внимание – здесь HTML шаблона нет, 
+    # обратите внимание – здесь HTML шаблона нет,
     # возвращается просто текст
     current_time = datetime.now()
-    template_name = 'app/current_time.html'
-    # current_time = None
+    # template_name = 'app/current_time.html'
     msg = f'Текущее время: {current_time}'
-    return render(request, template_name, msg)
+    return HttpResponse(msg)
 
 
 def workdir_view(request):
     # по аналогии с `time_view`, напишите код,
-    # который возвращает список файлов в рабочей 
+    # который возвращает список файлов в рабочей
     # директории
-    list_ = os.listdir()
-    template_name = 'app/workdir.thml'
-    return render(request, template_name, list_)
+    list_ = os.listdir('.')
+
+    return HttpResponse(list_)
